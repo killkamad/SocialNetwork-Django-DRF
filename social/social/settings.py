b3b0f577fdfd79f7b749745e98bcb54d60a5d6aa
Kirill <killka_m@mail.ru>
2019-04-22 00:06:37 +0600

    fix

--- social/social/settings.py
+++ social/social/settings.py
@@ -140,14 +140,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "social/static"),
     #'/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 # STATIC_ROOT = '/Универ/Программирование/GitHub/SocialNet/social/assets'
 
 MEDIA_URL = "/media/"
