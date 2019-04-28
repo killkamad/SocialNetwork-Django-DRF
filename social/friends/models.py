2c5e7585ef7ab73cba050ebaa4e77b030493b451
Kirill <killka_m@mail.ru>
2019-04-29 01:05:51 +0600

    fix

--- social/friends/models.py
+++ social/friends/models.py
@@ -1,18 +1,23 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
-
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     friends = models.ManyToManyField("Profile", blank=True)
 
     def __str__(self):
         return str(self.user.username)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.user)
+        super(Profile, self).save(*args, **kwargs)
+
     def get_absolute_url(self):
-    	return "/users/{}".format(self.slug)
+        return "/users/{}".format(self.slug)
 
 
 def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
@@ -22,13 +27,14 @@ def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
         except:
             pass
 
+
 post_save.connect(post_save_user_model_receiver, sender=settings.AUTH_USER_MODEL)
 
 
 class FriendRequest(models.Model):
-	to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to_user', on_delete=models.CASCADE)
-	from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='from_user',  on_delete=models.CASCADE)
-	timestamp = models.DateTimeField(auto_now_add=True) # set when created
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to_user', on_delete=models.CASCADE)
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='from_user', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)  # set when created
 
-	def __str__(self):
-		return "From {}, to {}".format(self.from_user.username, self.to_user.username)
+    def __str__(self):
+        return "From {}, to {}".format(self.from_user.username, self.to_user.username)
