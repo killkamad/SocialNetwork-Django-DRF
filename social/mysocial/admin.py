4e32f806139824b95dce4277dad2eb2dc942aad7
Kirill <killka_m@mail.ru>
2019-04-30 00:21:48 +0600

    da

--- social/mysocial/admin.py
+++ social/mysocial/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Like
 # from .models import Profile, FriendRequest
 #
 # admin.site.register(Profile)
 # admin.site.register(FriendRequest)
 
 admin.site.register(Post)
+admin.site.register(Like)
